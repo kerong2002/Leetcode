d032d84fbb63551db975d8c5b0a1a104309e9eb9
kerong <70834651+kerong2002@users.noreply.github.com>
2023-04-22 15:58:35 +0800

    Add files via upload

---
+++ C++/0035. Search Insert Position.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size()-1;
+        int mid = 0;
+        while(left <= right){
+            mid = (left + right)/2;
+            if(target > nums[mid]){
+                left = mid + 1;
+            }
+            else if(target < nums[mid]){
+                right = mid-1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return left;
+    }
+};
