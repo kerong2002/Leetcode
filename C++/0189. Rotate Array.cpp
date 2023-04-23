9be205cb6c40245f2e745e07af7235a7c08cffba
kerong <70834651+kerong2002@users.noreply.github.com>
2023-04-23 14:33:22 +0800

    Add files via upload

--- C++/0189. Rotate Array.cpp
+++ C++/0189. Rotate Array.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
+    void reverse(vector<int>&nums, int left, int right){
+        while(left < right){
+            swap(nums[left], nums[right]);
+            left++;
+            right--;
+        }
+        return;
+    }
     void rotate(vector<int>& nums, int k) {
-        vector<int> save = nums;
-        int r = nums.size()- k%nums.size();
-        int l = 0;
-        for(;r<nums.size();r++,l++){
-            nums[l] = save[r];
-        }
-        r = 0;
-        for(;l<nums.size();l++,r++){
-            nums[l] = save[r];
-        }
+        reverse(nums, 0  , nums.size()-1);
+        reverse(nums, 0  , k%nums.size()-1);
+        reverse(nums, k%nums.size()  , nums.size()-1);
         return;
     }
 };
