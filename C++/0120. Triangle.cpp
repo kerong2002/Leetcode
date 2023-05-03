4eaf51d865d7e2460962f71fe1d30e4e9ad7cbbe
kerong <70834651+kerong2002@users.noreply.github.com>
2023-05-03 15:04:27 +0800

    Add files via upload

--- C++/0120. Triangle.cpp
+++ C++/0120. Triangle.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int row = triangle.size();
-        int col = triangle[row].size();
+        int col = triangle[0].size();
         for(int y=row-2;y>=0;y--){
             for(int x=y;x>=0;x--){
                 triangle[y][x] += min(triangle[y+1][x], triangle[y+1][x+1]);
