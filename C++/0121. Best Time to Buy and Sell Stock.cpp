d4243e193ae99be330941f98f60d175d49c7539c
kerong <70834651+kerong2002@users.noreply.github.com>
2023-06-26 19:54:41 +0800

    Add files via upload

--- C++/0121. Best Time to Buy and Sell Stock.cpp
+++ C++/0121. Best Time to Buy and Sell Stock.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int result = 0;
-        int ans = 0;
-        for(int x=1;x<prices.size();x++){
-            result += prices[x]-prices[x-1];
-            ans = max(ans, result);
-            if(result <0){
-                result =0;
-            }
+        int size = prices.size();
+        vector<vector<int>> dp(size, vector<int>(2, 0));
+        dp[0][0] = -prices[0];
+        for (int x = 1; x < size; ++x) {
+            dp[x][0] = max(dp[x - 1][0], -prices[x]);
+            dp[x][1] = max(dp[x - 1][1], dp[x][0] + prices[x]);
         }
-        return ans;
+        return dp[size - 1][1];
     }
 };
