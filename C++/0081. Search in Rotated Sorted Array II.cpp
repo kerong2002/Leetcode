2d37e0456e3059fdc7c7195ec4d6a7b662f7ffb9
kerong <70834651+kerong2002@users.noreply.github.com>
2023-08-10 15:52:01 +0800

    Add files via upload

---
+++ C++/0081. Search in Rotated Sorted Array II.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int L = 0;
+        int R = nums.size() - 1;
+        while (L <= R) {
+            int mid = (L + R) / 2;
+            cout << mid <<"@\n";
+            if (nums[mid] == target) {
+                return true;
+            }
+            if(nums[L] == nums[mid] && nums[R] == nums[mid]) {
+                ++L;
+                --R;
+            }
+            else if (nums[L] <= nums[mid]) {
+                if (nums[L] <= target && nums[mid] > target) {
+                    R = mid - 1;
+                }
+                else {
+                    L = mid + 1;
+                }
+            }
+            else {
+                if (nums[mid] < target && nums[R] >= target) {
+                   L = mid + 1;
+                }
+                else {
+                    R = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+};
