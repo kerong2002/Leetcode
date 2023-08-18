e1c9fbdbabc05da39eee5b43a21981c28f74d0e6
kerong <70834651+kerong2002@users.noreply.github.com>
2023-08-18 14:02:50 +0800

    Add files via upload

--- C++/0242. Valid Anagram.cpp
+++ C++/0242. Valid Anagram.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+        int eng[26] = {0};
+        for (auto x : s) {
+            ++eng[x - 'a'];
+        }
+
+        for (auto x : t) {
+            --eng[x - 'a'];
+        }
+
+        for (auto x : eng) {
+            if (x != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 };
