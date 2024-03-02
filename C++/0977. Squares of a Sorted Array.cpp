1f37347803eb64ab1f28e50215b771719c831660
kerong <70834651+kerong2002@users.noreply.github.com>
2024-03-02 10:25:04 +0800

    Add files via upload

--- C++/0977. Squares of a Sorted Array.cpp
+++ C++/0977. Squares of a Sorted Array.cpp
@@ -1,10 +1,26 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for(int x=0;x<nums.size();x++){
-            nums[x] = nums[x]*nums[x];
+        vector<int> ans;
+        for (auto x : nums) {
+            ans.push_back(pow(x, 2));
         }
-        sort(nums.begin(),nums.end());
-        return nums;
+        for (int i = 0; i< ans.size(); ++i) {
+            if (i % 2 == 0) {
+                for (int x = 0; x < ans.size() - 1; x += 2) {
+                    if (ans[x] > ans[x + 1]) {
+                        swap(ans[x], ans[x + 1]);
+                    }
+                }
+            }
+            else {
+                for (int x = 1; x < ans.size() - 1; x += 2) {
+                    if (ans[x] > ans[x + 1]) {
+                        swap(ans[x], ans[x + 1]);
+                    }
+                }
+            }
+        }
+        return ans;
     }
 };
